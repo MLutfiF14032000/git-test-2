6c98e0020aef4ba2d16eb105215ed689d01ab859
Moh Lutfi Fauzi <mlfauzi54@gmail.com>
2020-02-22 03:39:56 +0700

    Menambahkan lorem

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi amet officia dignissimos vitae, officiis maxime libero suscipit voluptatum doloribus debitis accusantium consequatur mollitia ratione, cum quia odit, tenetur quod ipsa.</h3>
 </body>
 </html>
